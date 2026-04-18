7c7a89a3d0742ed40047795dda0c028535d70e84
Uliana Blinova <ulanablinova0@gmail.com>
2026-04-18 21:46:52 +0700

    Authentication function

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -1,34 +1,45 @@
 <?php
+
 namespace Src;
+
 use Error;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Src\Auth\Auth;
 
 class Application
 {
-  private Settings $settings;
-  private Route $route;
-  private Capsule $dbManager;
- public function __construct(Settings $settings)
+   private Settings $settings;
+   private Route $route;
+   private Capsule $dbManager;
+   private Auth $auth;
+
+   public function __construct(Settings $settings)
    {
        $this->settings = $settings;
        $this->route = new Route($this->settings->getRootPath());
        $this->dbManager = new Capsule();
-   }
-public function __get($key)
-{
-   switch ($key) {
-       case 'settings':
-           return $this->settings;
-       case 'route':
-           return $this->route;
-       default:
-           throw new Error('Accessing a non-existent property');
-   }
-}
+       $this->auth = new $this->settings->app['auth'];
 
- private function dbRun()
+       $this->dbRun();
+       $this->auth::init(new $this->settings->app['identity']);
+   }
+
+   public function __get($key)
+   {
+       switch ($key) {
+           case 'settings':
+               return $this->settings;
+           case 'route':
+               return $this->route;
+           case 'auth':
+               return $this->auth;
+       }
+       throw new Error('Accessing a non-existent property');
+   }
+
+   private function dbRun()
    {
        $this->dbManager->addConnection($this->settings->getDbSetting());
        $this->dbManager->setEventDispatcher(new Dispatcher(new Container));
@@ -38,8 +49,6 @@ public function __get($key)
 
    public function run(): void
    {
-       $this->dbRun();
-       $this->route->setPrefix($this->settings->getRootPath());
        $this->route->start();
    }
 }
