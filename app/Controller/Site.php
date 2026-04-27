b7171d429592437e2c77e29af34080959fabbd3d
Uliana Blinova <ulanablinova0@gmail.com>
2026-04-27 15:57:28 +0700

    Middleware Integration

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,8 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
+
 class Site
 {
    public function index(Request $request): string
@@ -24,6 +26,22 @@ class Site
        app()->route->redirect('/go');
    }
    return new View('site.signup');
+}
+public function login(Request $request): string
+{
+   if ($request->method === 'GET') {
+       return new View('site.login');
+   }
+   if (Auth::attempt($request->all())) {
+       app()->route->redirect('/hello');
+   }
+   return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+}
+
+public function logout(): void
+{
+   Auth::logout();
+   app()->route->redirect('/hello');
 }
 
 
